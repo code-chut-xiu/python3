2f3a6a23b189218fa051428e035a03ca46bd98d6
Hung Le <the.bakonator@outlook.com>
2020-02-02 18:24:27 -0700

    Added string_functions file

---
+++ string_functions.py
@@ -0,0 +1,22 @@
+ss = "Sammy Shark!"
+print(ss.upper())
+
+print(ss.lower())
+
+number = "5"
+letters = "abcdef"
+
+# Should be True
+print("{} is alphanumeric is {}".format(number, number.isalnum()))
+# Should be True
+print("{} is alphanumeric is {}".format(letters, letters.isalnum()))
+# Should be False
+print("{} is alphabetical is {}".format(number, number.isalpha()))
+# Should be True
+print("{} is alphabetical is {}".format(letters, letters.isalpha()))
+
+movie = "2001: A SAMMY ODYSSEY"
+book = "A Thousand Splendid Sharks"
+poem = "sammy lived in a pretty how town"
+
+
